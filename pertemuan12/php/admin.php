4663e73afad1ad3231cec222b28e50f7e701c58f
Nikolasramadhan06 <nikolasramadhan0612gmail.com>
2021-05-14 11:43:43 +0700

    menyelesaikan tugas

--- pertemuan12/php/admin.php
+++ pertemuan12/php/admin.php
@@ -14,7 +14,7 @@ $mahasiswa = query("SELECT * FROM Mahasiswa");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dealer Mobil Jaya</title>
+    <title>Mahasiswa</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bulma.min.css">
